4993b33615e860223cdcde8e928fc713fdb0535f
Cleopatra <piperschwartz@aol.com>
2019-11-17 22:45:26 -0700

    work in progress

---
+++ app.js
@@ -0,0 +1,17 @@
+//2 empty arrays
+//holds team members
+//holds ID's
+
+//create manager function 
+  // inquirer promts
+      //.then
+      //create manager from Manager Class
+      //push into team array
+      //push ID into ID array
+      //fire createTeam function
+
+// create team function
+    //Inquirer prompts with 3 choices
+        //.then
+        //switch case to either add engineer, intern, or build team
+        //each member is posted into team array and ID array  
